db6e4337f218a2f46b967ad0128bb81a233c97ab
GoshaBlokhin <blohinmine@gmail.com>
2025-02-22 02:55:32 +0300

    Added product_detail_page with unique link for every product

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("logout/", views.logout_page, name="logout_page"),
     path("product-add/", views.product_add_page, name="product_add_page"),
     path("product-list/", views.product_list_page, name="product_list_page"),
+    path("product-detail/<int:product_id>/", views.product_detail, name="product_detail"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from pyexpat.errors import messages
 from django.contrib import messages
 from django.contrib.auth import login, logout
@@ -97,3 +97,11 @@ def product_list_page(request):
         return redirect("login_page")
     else:
         return render(request, 'product_list_page.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.user.is_authenticated:
+        return render(request, 'product_detail_page.html', {'product': product})
+    else:
+        return redirect("login_page")
