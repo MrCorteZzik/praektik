e7b7f16fbc0e0468389237d25e31e1fcd1ddb37c
GoshaBlokhin <blohinmine@gmail.com>
2025-02-24 01:22:42 +0300

    small fix

--- main_app/views.py
+++ main_app/views.py
@@ -155,7 +155,10 @@ def product_remove_one_from_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
 
     cart_item = CartItem.objects.get(user=request.user, product=product)
-    cart_item.quantity -= 1 if cart_item.quantity > 1 else cart_item.delete()
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+    else:
+        cart_item.delete()
     cart_item.save()
 
     return redirect("cart_page")
