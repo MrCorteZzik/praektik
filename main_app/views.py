ccdaa8b645bbf90482aaa3d794fdc41303724473
GoshaBlokhin <blohinmine@gmail.com>
2025-02-24 01:22:50 +0300

    small fix

--- main_app/views.py
+++ main_app/views.py
@@ -170,7 +170,10 @@ def product_add_one_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
 
     cart_item = CartItem.objects.get(user=request.user, product=product)
-    cart_item.quantity += 1 if cart_item.quantity < product.stock else messages.error(request, "Недостаточно товара в наличии!")
+    if cart_item.quantity < product.stock:
+        cart_item.quantity += 1
+    else:
+        messages.error(request, "Недостаточно товара в наличии!")
     cart_item.save()
 
     return redirect("cart_page")
